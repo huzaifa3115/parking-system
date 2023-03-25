47140bf9bb9ffb54bbd75b48fe1ede5d340c5f13
huzaifa3115 <sharpcreation3115@gmail.com>
2023-03-25 16:39:00 +0500

    login route

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -1,5 +1,9 @@
+const { login } = require("../controllers/user");
+
 module.exports = function (app) {
   app.get("/test", (req, res) => {
-    res.send('OK');
+    res.send("OK");
   });
+
+  app.post("login", login);
 };
